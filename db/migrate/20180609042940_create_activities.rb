f04f82534590d4b40cdf205495102ee63754dc01
Ushinji <siguma971@gmail.com>
2018-06-09 13:41:05 +0900

    Store, Menu, activityテーブルの作成

---
+++ db/migrate/20180609042940_create_activities.rb
@@ -0,0 +1,18 @@
+class CreateActivities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :activities do |t|
+      t.references :user, null: false
+      t.bigint :store_id, null: false
+      t.bigint :menu_id, null: false
+      t.string :size, null: false
+      t.string :yasai, null: false
+      t.string :ninniku, null: false
+      t.string :abura, null: false
+      t.string :karame, null: false
+      t.string :image_url
+      t.integer :calorie
+      
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_09_041909) do
+ActiveRecord::Schema.define(version: 2018_06_09_042940) do
+
+  create_table "activities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "store_id", null: false
+    t.bigint "menu_id", null: false
+    t.string "size", null: false
+    t.string "yasai", null: false
+    t.string "ninniku", null: false
+    t.string "abura", null: false
+    t.string "karame", null: false
+    t.string "image_url"
+    t.integer "calorie"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "menus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false

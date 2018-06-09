4d85c62c2fdbd1a13b47fcd680ef315c3f42aa36
Ushinji <siguma971@gmail.com>
2018-06-09 13:47:02 +0900

    シードファイルに、ユーザ作成を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+user = User.create(name: 'hirao', email: 'hirao@mail.com')
 store = Store.create(name: '三田本店')
 Menu.create(name: 'ぶた入りラーメン')
 Menu.create(name: 'ぶたダブルラーメン')

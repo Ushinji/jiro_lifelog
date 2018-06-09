b069fe230399e1fbeb87f64175bb202fd2d383a4
maxpowerti <maxpowerti@live.jp>
2018-06-09 16:01:34 +0900

    Modalでのボタン押下後の処理追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
     def new
       @user = User.new
     end
+    def create
+      p params
+        @user = User.new
+        name =params['user']['name']
+        email =params['user']['email']
+        @user.name= name
+        @user.email=email
+
+      respond_to do |format|
+        if @user.save
+          format.html { redirect_to @user, notice: 'User was successfully created.' }
+          format.json { render :show, status: :created, location: @user }
+          format.js { @status = "success"}
+        else
+          format.html { render :new }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+          format.js { @status = "fail" }
+        end
+      end
+    end 
   end

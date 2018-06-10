939280441ae20aa0a2451aaba5390888ffeb0ef5
Ushinji <siguma971@gmail.com>
2018-06-10 10:39:58 +0900

    Userのemailにユニーク制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_09_042940) do
+ActiveRecord::Schema.define(version: 2018_06_10_013844) do
 
   create_table "activities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_06_09_042940) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

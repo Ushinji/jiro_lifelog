3e56df9a44e6fa3ea2b6b4e977e167622213feb0
Ushinji <siguma971@gmail.com>
2019-06-01 17:25:14 +0900

    Userのバリデーションに関するテストを追加

---
+++ spec/models/users_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe 'validation' do
+    subject { build(:user) }
+    
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+  end
+end

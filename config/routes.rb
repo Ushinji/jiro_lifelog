d28dfe9f13aabb31b5fb06c7a2ef377e9b768ad9
Ushinji <siguma971@gmail.com>
2019-06-08 23:16:44 +0900

    ルートディレクトリをSPAに向けるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'root#index'
-
-  match "/dashboard", to: "dashboard#index", via: :all
-  match "/dashboard/*any", to: "dashboard#index", via: :all
+  # match "/dashboard", to: "dashboard#index", via: :all
+  # match "/dashboard/*any", to: "dashboard#index", via: :all
 
   get "/login", to: "sessions#login"
   get "/auth/google/callback", to: "sessions#callback"
-  resources :users
-  resources :activities
+  
+  root to: 'dashboard#index'
+  match "*any", to: "dashboard#index", via: :all
+
+  # resources :users
+  # resources :activities
 end

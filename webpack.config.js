2c009e0d5c19fa774f3fff2a673c837abffa5336
Ushinji <siguma971@gmail.com>
2019-06-08 21:51:50 +0900

    vue.jsのstyleでscssを使えるようにした

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
         test: /\.css$/,
         use: ['vue-style-loader', 'css-loader'],
       },
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   plugins: [new ManifestPlugin(), new VueLoaderPlugin()],

d77933988fee12d7422514b14b2918cf14910a5a
Ushinji <siguma971@gmail.com>
2019-06-08 20:38:20 +0900

    Dashboardページコンポーネントの作成

---
+++ src/components/pages/DashboardPage/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>ダッシュボードページ</h1>
+    <p>ほげほげ</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashboardPage',
+};
+</script>
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,18 +1,8 @@
-import Top from './pages/Top';
-import Hello from './pages/Hello';
-import Counter from './pages/Counter';
+import DashboardPage from './pages/DashboardPage/index';
 
 export default [
   {
-    path: '/',
-    component: Top,
-  },
-  {
-    path: '/hello',
-    component: Hello,
-  },
-  {
-    path: '/counter',
-    component: Counter,
+    path: '/dashboard',
+    component: DashboardPage,
   },
 ];

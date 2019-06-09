dc5c79ff7fe4c091a7ca1e81c6e85b9cb19a2de0
Ushinji <siguma971@gmail.com>
2019-06-09 15:02:35 +0900

    Panelコンポーネントを作成

---
+++ src/components/atoms/Panel/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="panel">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Panel',
+};
+</script>
+
+<style lang="scss" scoped>
+.panel {
+  background-color: white;
+  border: 1px solid #f1f1f1;
+}
+</style>

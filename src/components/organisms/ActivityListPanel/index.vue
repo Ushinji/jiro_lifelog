78d855387fd4704bc9eaa42e5316358b7894c94e
Ushinji <siguma971@gmail.com>
2019-06-13 23:00:48 +0900

    ActivityListコンポーネントの仮作成

---
+++ src/components/organisms/ActivityListPanel/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <Panel>
+    <div>
+      <h1>ActivityList</h1>
+      <p>ほげ</p>
+    </div>
+  </Panel>
+</template>
+
+<script>
+import Panel from '@/components/atoms/Panel';
+
+export default {
+  name: 'ActivityListPanel',
+  components: {
+    Panel,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

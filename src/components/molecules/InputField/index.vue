805863e10883a4b2688ae7e9de835dfa320238db
Ushinji <siguma971@gmail.com>
2019-07-03 22:48:06 +0900

    Formのslectフィールドコンポーネント実装

---
+++ src/components/molecules/InputField/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="input-field">
+    <label>{{ label }}</label>
+    <input :value="value" :type="inputtype" />
+    <div v-if="isError" class="input-field--error-msg">
+      ErrorMsg
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputField',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    inputtype: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    isError: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {},
+};
+
+/*
+<div class="input-field">
+    <label>店名</label>
+    <input v-model="form.storeName.value" />
+    <div v-if="form.storeName.isError" class="input-field--error-msg">
+    {{ form.storeName.errorMessage }}
+    </div>
+</div>
+*/
+</script>
+
+<style lang="scss" scoped>
+@import '@/components/styles/_colors.scss';
+
+.input-field {
+  font-size: 16px;
+
+  &--error-msg {
+    color: $red;
+  }
+}
+</style>
--- src/components/organisms/ActivityFormPanel/index.vue
+++ src/components/organisms/ActivityFormPanel/index.vue
@@ -1,17 +1,154 @@
 <template>
   <Panel>
-    活動記録を作成する
+    <div class="activity-form">
+      <div class="activity-form--header">
+        活動記録を作成する
+      </div>
+      <form>
+        <div class="form--field-group">
+          <div class="field-label">
+            麺の量
+          </div>
+          <SelectField
+            v-model="size"
+            label="麺の量を選択"
+            :options="sizeOptions"
+          />
+        </div>
+
+        <div class="form--field-group">
+          <div class="form--field-label">
+            野菜の量
+          </div>
+          <SelectField
+            v-model="yasai"
+            label="野菜の量を選択"
+            :options="yasaiOptions"
+          />
+        </div>
+
+        <div class="form--field-group">
+          <div class="form--field-label">
+            にんにくの量
+          </div>
+          <SelectField
+            v-model="ninniku"
+            label="にんにくの量を選択"
+            :options="ninnikuOptions"
+          />
+        </div>
+
+        <div class="form--field-group">
+          <div class="form--field-label">
+            カラシの量
+          </div>
+          <SelectField
+            v-model="karame"
+            label="カラシの量を選択"
+            :options="karameOptions"
+          />
+        </div>
+      </form>
+    </div>
   </Panel>
 </template>
 
 <script>
 import Panel from '@/components/atoms/Panel';
+import SelectField from '@/components/molecules/SelectField';
 
 export default {
   name: 'ActivityFormPanel',
   components: {
     Panel,
+    SelectField,
   },
   props: {},
+  data() {
+    return {
+      storeId: '',
+      menuId: '',
+      size: '',
+      yasai: '',
+      ninniku: '',
+      abura: '',
+      karame: '',
+    };
+  },
+  computed: {
+    sizeOptions: () => [
+      {
+        id: 1,
+        value: 'small',
+        label: '小',
+      },
+      {
+        id: 2,
+        value: 'large',
+        label: '大',
+      },
+    ],
+    yasaiOptions: () => [
+      {
+        id: 1,
+        value: 'small',
+        label: '小',
+      },
+      {
+        id: 2,
+        value: 'normal',
+        label: 'ノーコール',
+      },
+      {
+        id: 3,
+        value: 'large',
+        label: 'マシ',
+      },
+      {
+        id: 4,
+        value: 'mashimashi',
+        label: 'マシマシ',
+      },
+    ],
+    karameOptions: () => [
+      {
+        id: 1,
+        value: 'small',
+        label: '小',
+      },
+      {
+        id: 2,
+        value: 'normal',
+        label: 'ノーコール',
+      },
+      {
+        id: 3,
+        value: 'large',
+        label: 'マシ',
+      },
+      {
+        id: 4,
+        value: 'mashimashi',
+        label: 'マシマシ',
+      },
+    ],
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+@import '@/components/styles/_colors.scss';
+@import '@/components/styles/_form.scss';
+
+.activity-form {
+  padding: 16px;
+  width: 480px;
+
+  &--header {
+    font-size: 16px;
+    font-weight: bold;
+    color: $black60;
+    margin-bottom: 32px;
+  }
+}
+</style>
